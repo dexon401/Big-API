d205e7426148a34fed76f5dc94944e1e84512b66
dexon401 <255047233+dexon401@users.noreply.github.com>
2026-04-01 15:50:24 +0300

    Now with map controls

--- main.py
+++ main.py
@@ -19,15 +19,18 @@ MAP_FILE = "map.png"
 
 MAX_SPN = 10
 MIN_SPN = 0.002
+MAX_LAT = 58
+MIN_LAT = 53
+MAX_LON = 40
+MIN_LON = 35
 
 
 class GameView(arcade.Window):
     def setup(self):
         self.lat, self.lon = 55.75105603488043, 37.61748581976496
         self.spn = [0.3, 0.3]
-        
-        self.update_image()
 
+        self.update_image()
 
     def on_draw(self):
         self.clear()
@@ -46,11 +49,12 @@ class GameView(arcade.Window):
         params = {
             "apikey": STATIC_MAPS_API_KEY,
             "ll": f"{self.lon},{self.lat}",
-            "spn": ",".join(map(str,self.spn))
+            "spn": ",".join(map(str, self.spn)),
         }
         response = requests.get(STATIC_MAPS_URL, params=params)
 
         if not response:
+            print(params)
             print("Http статус:", response.status_code, "(", response.reason, ")")
             sys.exit(1)
 
@@ -58,9 +62,9 @@ class GameView(arcade.Window):
             file.write(response.content)
 
         self.background = arcade.load_texture(MAP_FILE)
-        
+
         self.on_draw()
-        
+
     def on_key_press(self, symbol, modifiers):
         changed = False
         if symbol == arcade.key.PAGEUP:
@@ -74,11 +78,31 @@ class GameView(arcade.Window):
                 self.spn = new_spn
                 changed = True
 
+        if symbol == arcade.key.UP:
+            new_lat = min(self.lat + self.spn[0] * 0.45, MAX_LAT)
+            if new_lat != self.lat:
+                self.lat = new_lat
+                changed = True
+        if symbol == arcade.key.DOWN:
+            new_lat = max(self.lat - self.spn[0] * 0.45, MIN_LAT)
+            if new_lat != self.lat:
+                self.lat = new_lat
+                changed = True
+        if symbol == arcade.key.RIGHT:
+            new_lon = min(self.lon + self.spn[1] * 0.45, MAX_LON)
+            if new_lon != self.lon:
+                self.lon = new_lon
+                changed = True
+        if symbol == arcade.key.LEFT:
+            new_lon = max(self.lon - self.spn[1] * 0.45, MIN_LON)
+            if new_lon != self.lon:
+                self.lon = new_lon
+                changed = True
+
         if changed:
             print("Changing...")
             self.update_image()
-            
-        
+
         return super().on_key_press(symbol, modifiers)
 
 
